dc2dcfd3bae5a7ec3b3b0b0e8b1cdb491b3e44db
Bruno Correa Fanga <30472235+brunozxt@users.noreply.github.com>
2018-04-23 00:15:24 -0300

    Update heroiSchema.js

--- heroiSchema.js
+++ heroiSchema.js
@@ -1,12 +1,4 @@
-// https://www.youtube.com/watch?v=pWT-KF0Z2fA&t=102s
-// https://www.facebook.com/groups/h4treinamentos/
 
-// https://www.facebook.com/events/2027744037493186/
-
-
-// instalamos o Mongoose para validar e gerenciar
-// modelos de dados do MongoDB
-// npm i --save mongoose
 const mongoose = require("mongoose");
 const Schema = new mongoose.Schema({
     nome: {
@@ -36,11 +28,7 @@ const model = mongoose.model("heroi", Schema);
     connection.once("open", () => console.log("banco rodando!!"));
     connection.once("error", e => console.error("Deu Ruim!", e));
     module.exports = model;
-    //     await model.create({
-    //         nome: 'Sherlock',
-    //         poder: 'eqwr',
-    //         fraqueza: "fraco",
-    //     })
-    //     console.log("result", await model.findOneAndRemove())
+    
+    
 
 })();
